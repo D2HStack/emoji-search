8de455e4b33ad24d6a4d011cf4a9eaff37cab4bf
Hoang <D2HStack@users.noreply.github.com>
2020-02-06 21:26:00 +0100

    with copy to clipboard

--- src/components/Line.js
+++ src/components/Line.js
@@ -4,7 +4,30 @@ function Line(props) {
   const { symbol, title } = props;
   return (
     <>
-      <li className="line">
+      <li
+        className="line"
+        onClick={() => {
+          console.log(symbol);
+          // create a dummy html tag input with value symbol
+          let dummy = document.createElement("input");
+          document.body.appendChild(dummy);
+
+          // Set its ID
+          dummy.setAttribute("id", "dummy_id");
+
+          // Output the array into it
+          document.getElementById("dummy_id").value = symbol;
+
+          // Select it
+          dummy.select();
+
+          // Copy its contents
+          document.execCommand("copy");
+
+          // Remove it as its not needed anymore
+          document.body.removeChild(dummy);
+        }}
+      >
         <span>{symbol}</span>
         <span className="title">{title}</span>
       </li>
